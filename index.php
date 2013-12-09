4574140946a276ee7a412644e0ca3c36c34ba9f3
OmniscientLegacy <machmovies@gmail.com>
2013-12-08 20:11:02 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<center><a href="#game">
+<img src="http://i43.tinypic.com/new6yp.gif" alt=""></a></p><a href="#chat">
+<img src="chat.gif" alt=""></a></p>
+<center><a id="chat"><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+ <meta name="language" content="de" />
+ <meta name="author" content="Valentin Manthei - lightIRC.com" />
+ <title>PokeEmu Online</title>
+ <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>
+ <script type="text/javascript" src="config.js"></script>
+ <style type="text/css">
+	html { height: 95%; overflow: hidden; }
+	body { height: 95%;	margin:0;	padding:0; background-color:#999;	}
+ </style>
+</head>
+
+
+
+<body>
+ <div id="lightIRC" style="height:100%; text-align:center;">
+  <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
+ </div>
+ 
+ <script type="text/javascript">
+	swfobject.embedSWF("lightIRC.swf", "lightIRC", "95%", "95%", "10.0.0", "expressInstall.swf", params);
+ </script>
+</body>
+</html>
+</a>
+</center>
+<center>
+<html>
+<body>
+<b><font color=red>Created by: Yuuta Kirishima</b>
+</center>
